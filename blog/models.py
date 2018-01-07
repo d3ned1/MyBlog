bca43fc87e1254c6d229114b527a4ea9f46b478b
denis <1@g,.com>
2018-01-07 12:05:55 +0300

    Just commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from statistics import mode
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    user = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+    )
